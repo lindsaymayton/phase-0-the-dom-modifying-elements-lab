1fd792320e21b8765cbe538477f9c9e345a3e519
LindsayMayton <lindsaymayton@yahoo.com>
2023-10-03 15:10:19 -0700

    modifiedElement

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+document.addEventListener('DOMContentLoaded', () => {
+        const mainNode = document.querySelector('main#main');
+        mainNode.remove(); 
+        const main = document.querySelector('main#main');
+main.remove();
+
+const headerNode = document.querySelector('h1');
+console.log(mainNode);
+  console.log(headerNode);
+});
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Lindzdogg is the champion';
+document.body.appendChild(newHeader);
